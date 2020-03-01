fee8ba044830d92b2b35205d3d2ce08cbb1216e8
karanja477git config --global user.email karanjakaruga89@gmail.com <karanjakaruga89@gmail.com>
2020-03-01 09:08:06 +0300

    delete and started again

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,83 @@
-$(function() {
-    $('.summary').hide();
-    $('.cdata-overlay').hide();
+function getSizeCost() {
+    var selectedSize = document.getElementById("size").value;
+    return parseInt(selectedSize);
+}
 
-//Get inputs
-    $("#checkout").click(function() {
-        let flavour = $(".flavour option:selected").val();
-        let size = $("#size option:selected").val();
-        let crust = $("#crust option:selected").val();
-        let toppings = $("#toppings option:selected").val();
-        let number = $("#number").val();
+function getCrustCost() {
+    var selectedCrust = document.getElementById("crust").value;
+    return parseInt(selectedCrust);
+}
+function getNumber() {
+    var selectedNumber = document.getElementById("numberofpizza").value;
+    return parseInt(selectedNumber);
+}
+function mushrooms() {
+    var mushroom = 0;
+    var addMushroom = document.getElementById("toppingone");
+    if (addMushroom.checked === true) {
+        mushroom = 150;
+    }
+    return parseInt(mushroom);
+}
+
+function sausages() {
+    var sausage = 0;
+    var addsausage = document.getElementById("toppingone");
+    if (addsausage.checked === true) {
+        sausage = 140;
+    }
+    return parseInt(sausage);
+}
+function onions() {
+    var onion = 0;
+    var addonions = document.getElementById("toppingone");
+    if (addMushroom.checked === true) {
+        onion = 130;
+    }
+    return parseInt(onion);
+}
+function tikkaSaucee() {
+    var tikkaSauce = 0;
+    var addtikkaSauce = document.getElementById("toppingone");
+    if (addtikkaSauce.checked === true) {
+        tikkaSauce = 120;
+    }
+    return parseInt(tikkaSauce);
+}
+function mozarellaCheese() {
+    var cheese = 0;
+    var addmozarellaCheese = document.getElementById("toppingone");
+    if (addmozarellaCheese.checked === true) {
+        cheese = 110;
+    }
+    return parseInt(cheese);
+}
+
+function calctotalPrice(e) {
+    event.preventDefault();
+    var totalPrice = (getSizeCost() + getCrustCost() + mushrooms()) * (getNumber());
+    
+        console.log(totalPrice);
+    alert("Your order of " + getNumber() + " pizzas has been processed.Your total amount payable is " + totalPrice +"." )
+
+}
+
+
+
+// Delivery form inputs and alerts
+
+
+
+$(document).ready(function () {
+    $("#delivery").submit(function () {
+
+        var name = $("input#name").val();
+        var number = $("input#number").val();
+        var location = $("input#location").val();
+
+        alert("Hello " + name + ". Your order has been successfuly received and will be delivered to " +  location + " within one hour.The delivery will cost ksh 180/= Thank you for chosing the pizzeria.");
+        // $(this).get(0).reset();
+        //  event.preventDefault();
     });
-    let price, totalPrice;
-    switch (flavour) {
-        case flavour = "vegetikka":
-            switch (size) {
-                case size = "regular":
-                    price = 300;
-                    if (crust === "thin") {
-                        totalPrice = (price * number) + 100;
-                    } else if (crust === "thick") {
-                        totalPrice = (price * number) + 150;
-                    } else if (crust === "flatbread") {
-                        totalPrice = (price * number) + 200;
-                    } else if (crust === "deep") {
-                        totalPrice = (price * number) + 250;
-                    } else if (crust === "cheese") {
-                        totalPrice = (price * number) + 300;
-                    } else {
-                        totalPrice = (price * number) +350;
-                    }
-                    break;
-
-                case size = "medium":
-                    price = 600;
-                    if (Crust === "thin") {
-                        totalPrice =(price * number) + 100;
-                    }else if (crust === "thick") {
-                        totalPrice
-                    }
-                
-            }
-    };
-
+    
 });

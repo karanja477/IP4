2b02d6941912c68a7e0125d03e5949c4d82ab6a7
karanja477git config --global user.email karanjakaruga89@gmail.com <karanjakaruga89@gmail.com>
2020-03-01 09:18:22 +0300

    changed the spelling of labels

--- js/scripts.js
+++ js/scripts.js
@@ -75,7 +75,7 @@ $(document).ready(function () {
         var number = $("input#number").val();
         var location = $("input#location").val();
 
-        alert("Hello " + name + ". Your order has been successfuly received and will be delivered to " +  location + " within one hour.The delivery will cost ksh 180/= Thank you for chosing the pizzeria.");
+        alert("Hello " + name + ". Your order has been successfuly received and will be delivered to " +  location + " within one hour.The delivery will cost ksh 180/= Thank you for chosing the Delicacies Pizza.");
         // $(this).get(0).reset();
         //  event.preventDefault();
     });
